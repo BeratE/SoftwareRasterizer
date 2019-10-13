343a8ad76f5b317b13bd33c3cccb4d605ef87136
Berat Ertural <Beertural@gmail.com>
2019-10-13 21:36:46 +0200

    sdl2 support

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
- #include <iostream>
+#include <iostream>
+#include "SDL2/SDL.h"
 
 int main () {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow(
+        "Real-Time Graphics",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+	800, 600,
+	0);
 
-    int i = 0;
-
-    i++;
     std::cout << "Hello World" << std::endl;
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
     return 0;
 }
 
