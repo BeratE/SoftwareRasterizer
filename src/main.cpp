101de3b3cbfb271946b2d795777b3380b5aa834a
Berat Ertural <Beertural@gmail.com>
2019-10-18 21:51:00 +0200

    seperate window creation, opengl init

--- src/main.cpp
+++ src/main.cpp
@@ -14,22 +14,48 @@ static void sdl_die(const char * message) {
     exit(2);
 }
 
-void resize_window() {
+void resize_callback() {
     int w, h;
     SDL_GetWindowSize(_window, &w, &h);
     glViewport(0, 0, w, h);
 }
 
-void init_window() {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	sdl_die("Error initializing SDL");
-
+void init_gl () {
     SDL_GL_LoadLibrary(NULL);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, OPENGL_VERSION_MAJOR);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, OPENGL_VERSION_MINOR);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    _glContext = SDL_GL_CreateContext(_window);
+    if (_glContext == NULL) {
+	sdl_die("Failed to create OpenGL context");
+    }
+
+    gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+    
+    printf("OpenGL loaded, ");
+    printf("using %i.%i (%s)\n", OPENGL_VERSION_MAJOR, OPENGL_VERSION_MINOR, "Core");
+    printf("Current Device:\n");
+    printf("Vendor:   %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version:  %s\n", glGetString(GL_VERSION));
+    
+    SDL_GL_SetSwapInterval(1);
+
+    resize_callback();
+
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_CULL_FACE);
+    
+    glClearColor(1.0, 1.0, 0.0, 1.0);
+}
+
+void init_window() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	sdl_die("Error initializing SDL");
+    atexit(SDL_Quit);
     
     _window = SDL_CreateWindow(
         PROJECT_NAME,
@@ -39,30 +65,11 @@ void init_window() {
 
     if (_window == NULL)
 	sdl_die("Error setting video mode");
-
-    _glContext = SDL_GL_CreateContext(_window);
-    if (_glContext == NULL) {
-	sdl_die("Failed to create OpenGL context");
-    }
-
-    printf("OpenGL loaded\n");
-    gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
-    printf("Vendor:   %s\n", glGetString(GL_VENDOR));
-    printf("Renderer: %s\n", glGetString(GL_RENDERER));
-    printf("Version:  %s\n", glGetString(GL_VERSION));
-
-    SDL_GL_SetSwapInterval(1);
-
-    resize_window();
-    
-    glDisable(GL_DEPTH_TEST);
-    glDisable(GL_CULL_FACE);
-    
-    glClearColor(1.0, 0.0, 0.0, 1.0);
 }
 
 int main () {
     init_window();
+    init_gl();
 
     SDL_Event event;
     bool quit = false;
@@ -72,6 +79,7 @@ int main () {
 	    if (event.type == SDL_QUIT) 
 		quit = true;
 	}
+	glClear(GL_COLOR_BUFFER_BIT);
     }
     
     SDL_DestroyWindow(_window);
