8d9ad127d39cf355aa021c95e885d7a8635489e4
Ertural.Berat <ertural.berat@scheidt-bachmann.de>
2024-10-13 12:58:01 +0200

    Delete rudimentary files

--- include/config.h
+++
@@ -1,5 +0,0 @@
-// Cmake configuration settings
-#define ROOT_DIR "/home/berat/Projects/cepples/rtg/"
-#define PROJECT_NAME "SoftwareRasterizationEngine"
-#define PROJECT_VERSION_MAJOR 1
-#define PROJECT_VERSION_MINOR 0
